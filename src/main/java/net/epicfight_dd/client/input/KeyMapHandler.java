74a1a197b89e06e96890777a63fbc16dc89546bf
Sidtheplayer <140822160+Sidtheplayer@users.noreply.github.com>
2026-03-19 22:18:24 +0530

    cannot switch wing stance mid-attack now

--- src/main/java/net/epicfight_dd/client/input/KeyMapHandler.java
+++ src/main/java/net/epicfight_dd/client/input/KeyMapHandler.java
@@ -14,6 +14,8 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import yesman.epicfight.api.animation.AnimationPlayer;
+import yesman.epicfight.api.animation.types.AttackAnimation;
 import yesman.epicfight.client.world.capabilites.entitypatch.player.LocalPlayerPatch;
 import yesman.epicfight.skill.SkillSlots;
 import yesman.epicfight.world.capabilities.EpicFightCapabilities;
@@ -30,41 +32,46 @@ public class KeyMapHandler {
                 if(localPlayerPatch != null && localPlayerPatch.isEpicFightMode()
                   && localPlayerPatch.getSkill(SkillSlots.WEAPON_PASSIVE).getDataManager().hasData(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get())){
 
-                   if(localPlayerPatch.getSkill(SkillSlots.WEAPON_PASSIVE).getDataManager().getDataValue(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get()) == false){
-                       localPlayerPatch.getSkill(DawnDaySkills.WINGSTANCE).getDataManager().setDataSync(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get(),true);
+                    AnimationPlayer animationPlayer = localPlayerPatch.getAnimator().getPlayerFor(null);
 
-                       localPlayerPatch.getOriginal().playSound(SoundEvents.AMETHYST_BLOCK_RESONATE,150f,0.85f);
+                    if (animationPlayer != null && !(animationPlayer.getAnimation().checkType(AttackAnimation.class))){
 
-                       localPlayerPatch.getOriginal().playSound(SoundEvents.BEACON_ACTIVATE,150f,0.25f);
+                        if(localPlayerPatch.getSkill(SkillSlots.WEAPON_PASSIVE).getDataManager().getDataValue(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get()) == false){
+                            localPlayerPatch.getSkill(DawnDaySkills.WINGSTANCE).getDataManager().setDataSync(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get(),true);
 
+                            localPlayerPatch.getOriginal().playSound(SoundEvents.AMETHYST_BLOCK_RESONATE,150f,0.85f);
 
-                       for (int i = 1; i <= 5; i++) {
-                           mc.player.level().addParticle(
-                                   ParticleTypes.END_ROD,
-                                   mc.player.getX() - Math.min(0.25, mc.player.getRandomX(0.01)),mc.player.getY(), mc.player.getZ() + mc.player.getRandomZ(-0.01f), Math.min(mc.player.getRandom().nextFloat(), 0.6f), Math.min(mc.player.getRandom().nextFloat(), 0.2f) , Math.min(mc.player.getRandom().nextFloat(), 1.6f)
-                           );
-                           mc.player.level().addParticle(
-                                   ParticleTypes.END_ROD,
-                                   mc.player.getX() + Math.min(0.25, mc.player.getRandomX(0.01)),mc.player.getY(), mc.player.getZ() + mc.player.getRandomZ(0.01f),0.03, 0.020, 0.0
-                           );
-
-                       }
-
-                      localPlayerPatch.playAnimationSynchronized(WingStanceAnims.WINGSTANCE_TRANSITION,0.0f);
-
-                       ChangeLivingModifierPacket modifierPacket = new ChangeLivingModifierPacket(false);
-                       DDNetworkHandler.INSTANCE.sendToServer(modifierPacket);
+                            localPlayerPatch.getOriginal().playSound(SoundEvents.BEACON_ACTIVATE,150f,0.25f);
 
 
-                   } else {
-                       localPlayerPatch.getSkill(DawnDaySkills.WINGSTANCE).getDataManager().setDataSync(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get(), false);
-                       localPlayerPatch.getOriginal().playSound(SoundEvents.BEACON_DEACTIVATE, 70f,1.0f);
+                            for (int i = 1; i <= 5; i++) {
+                                mc.player.level().addParticle(
+                                        ParticleTypes.END_ROD,
+                                        mc.player.getX() - Math.min(0.25, mc.player.getRandomX(0.01)),mc.player.getY(), mc.player.getZ() + mc.player.getRandomZ(-0.01f), Math.min(mc.player.getRandom().nextFloat(), 0.6f), Math.min(mc.player.getRandom().nextFloat(), 0.2f) , Math.min(mc.player.getRandom().nextFloat(), 1.6f)
+                                );
+                                mc.player.level().addParticle(
+                                        ParticleTypes.END_ROD,
+                                        mc.player.getX() + Math.min(0.25, mc.player.getRandomX(0.01)),mc.player.getY(), mc.player.getZ() + mc.player.getRandomZ(0.01f),0.03, 0.020, 0.0
+                                );
 
-                       ChangeLivingModifierPacket modifierPacket = new ChangeLivingModifierPacket(true);
-                       DDNetworkHandler.INSTANCE.sendToServer(modifierPacket);
+                            }
+
+                           localPlayerPatch.playAnimationSynchronized(WingStanceAnims.WINGSTANCE_TRANSITION,0.0f);
+
+                            ChangeLivingModifierPacket modifierPacket = new ChangeLivingModifierPacket(false);
+                            DDNetworkHandler.INSTANCE.sendToServer(modifierPacket);
 
 
-                   }
+                        } else {
+                            localPlayerPatch.getSkill(DawnDaySkills.WINGSTANCE).getDataManager().setDataSync(SkillDataKeyZ.SPECIAL_STANCE_ACTIVATE.get(), false);
+                            localPlayerPatch.getOriginal().playSound(SoundEvents.BEACON_DEACTIVATE, 70f,1.0f);
+
+                            ChangeLivingModifierPacket modifierPacket = new ChangeLivingModifierPacket(true);
+                            DDNetworkHandler.INSTANCE.sendToServer(modifierPacket);
+
+
+                        }
+                    }
                 }
             }
         }
